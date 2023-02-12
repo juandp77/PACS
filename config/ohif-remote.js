9a6e1f068abd057167ce349caa275ba042b5dd7f
Juan Diego <juandp77@gmail.com>
2023-02-12 16:27:51 +0100

    Update ohif-remote.js

--- config/ohif-remote.js
+++ config/ohif-remote.js
@@ -8,9 +8,9 @@ window.config = {
     dicomWeb: [
       {
         name: 'Orthanc',
-        wadoUriRoot: 'https://orthanc.tarskipp.com:3335/wado',
-        qidoRoot: 'https://orthanc.tarskipp.com:3335/dicom-web',
-        wadoRoot: 'https://orthanc.tarskipp.com:3335/dicom-web',
+        wadoUriRoot: 'https://orthanc.tarskipp.com/wado',
+        qidoRoot: 'https://orthanc.tarskipp.com/dicom-web',
+        wadoRoot: 'https://orthanc.tarskipp.com/dicom-web',
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
         enableStudyLazyLoad: true,
